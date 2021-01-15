086e79e2cc1a9d19aa83b0c44ae4263577c9afa6
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-01-14 20:56:22 -0600

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user){
     // format the github api url
@@ -77,4 +78,31 @@ var formSubmitHandler = function(event) {
     }
   };
 
-  userFormEl.addEventListener("submit", formSubmitHandler)
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+            
+        }else{
+            alert("Error: " + response.statusText);
+        }
+    });
+  };
+
+  let buttonClickHandler = function(event){
+      let language = event.target.getAttribute("data-language")
+      console.log(language);
+
+      if(language){
+          getFeaturedRepos(language);
+
+          //clear old content
+          repoContainerEl.textContent = "";
+      }
+  }
+  userFormEl.addEventListener("submit", formSubmitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
